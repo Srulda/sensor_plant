9a72a2e043d7d5083fe38768a4d5286f7d40eb42
SpitzN <nadav.spitzer@gmail.com>
2019-05-29 09:54:53 +0300

    mobx store

---
+++ src/components/Layout/Loading.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Loading extends Component {
+  render() {
+    return (
+      <div class="spinner">
+        <div class="dot1" />
+        <div class="dot2" />
+      </div>
+    );
+  }
+}
+
+export default Loading;
