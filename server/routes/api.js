2e19731907c5a3945ee43ded274c073d0db0d504
chen <chenkamin@gmail.com>
2019-05-29 10:57:23 +0300

    fake data

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const Plants = require('../model/Plants')
-
+const MyPlants = require('../model/Myplants')
 
 router.get('/plants', function(req,res){
     Plants.find({}, function(err, result){
         res.send(result)
     })
 })
+
+router.get('/myPlants', function(req,res){
+    MyPlants.find({}, function(err, result){
+        res.send(result)
+    })
+})
+
+
+
 
 
 
